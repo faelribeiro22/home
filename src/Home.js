098ced1d5520e34c1f0322ade530c4a1d0014e3c
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-09-23 01:13:10 -0300

    fix: add input home

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
       <Jumbotron>
         <h1 className="display-3">Olá Module Federation</h1>
         <hr className="my-2" />
-        <p>Este componente é de outro app</p>
+        <p>Este componente é de outro app, que coisa incrivel</p>
+        <input type="text"/>
         <p className="lead">
           <Button color="primary" onClick={callFunction}>Botão</Button>
         </p>
