7b2da5bd18c5d266bfb725361579c5631fcfcbd5
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-09-23 00:24:24 -0300

    fix: remove jumbotron

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
 
 const Home = () => {
   const callFunction = () => console.log('Hello World');
   return (
     <div>
-      <Jumbotron>
         <h1 className="display-3">Olá Module Federation</h1>
         <hr className="my-2" />
         <p>Este componente é de outro app</p>
         <p className="lead">
           <Button color="primary" onClick={callFunction}>Botão</Button>
         </p>
-      </Jumbotron>
     </div>
   )
 }
